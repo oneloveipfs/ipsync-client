c866b6ebbc18188929203bfeb6a9422caa4429f5
techcoderx <engtianxi@outlook.com>
2020-03-09 22:27:45 +0800

    fix downloading

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const SocketIO = require('socket.io-client')
 const IPFSHttpClient = require('ipfs-http-client')
+const wget = require('wget-improved');
 const Config = require('./config.json')
 
 const IPFS = IPFSHttpClient({ host: 'localhost', port: '5001', protocol: 'http'})
-const { urlSource } = IPFSHttpClient
+const { globSource } = IPFSHttpClient
 
 if (Config.hashtypes.length === 0) console.log('Warning: selected hash types is empty, IPSync client will not pin any files.')
 
@@ -14,68 +15,100 @@ ipsync.on('message',(message) => {
 })
 
 ipsync.on('upload',async (data) => {
-    if (Config.users !== []) {
-        if (!Config.users.includes(data.username)) return
-    }
+    console.log('received',data)
 
     if (data.ipfshash) {
         // video upload
         if (Config.hashtypes.includes('videos')) {
-            for await (const file of IPFS.add(urlSource(Config.gateway + '/ipfs/' + data.ipfshash, { recursive: true }), { trickle: true })) {
-                console.log('pinned ' + file.cid.toString() + ' recursively')
-            }
+            downloadVideo(data.ipfshash,async () => {
+                for await (const file of IPFS.add(globSource(data.ipfshash, { recursive: true }), { trickle: true })) {
+                    console.log('pinned ' + file.cid.toString() + ' recursively')
+                }
+            })
         }
 
         if (Config.hashtypes.includes('sprites') && data.spritehash) {
-            for await (const file of IPFS.add(urlSource(Config.gateway + '/ipfs/' + data.spritehash, { recursive: true }), { trickle: true })) {
-                console.log('pinned ' + file.cid.toString() + ' recursively')
-            }
+            downloadVideo(data.spritehash,async () => {
+                for await (const file of IPFS.add(globSource(data.spritehash, { recursive: true }), { trickle: true })) {
+                    console.log('pinned ' + file.cid.toString() + ' recursively')
+                }
+            })
         }
 
         if (Config.hashtypes.includes('thumbnails') && data.snaphash) {
-            for await (const file of IPFS.add(urlSource(Config.gateway + '/ipfs/' + data.spritehash, { recursive: true }), { trickle: false })) {
-                console.log('pinned ' + file.cid.toString() + ' recursively')
-            }
+            downloadVideo(data.snaphash,async () => {
+                for await (const file of IPFS.add(globSource(data.snaphash, { recursive: true }), { trickle: false })) {
+                    console.log('pinned ' + file.cid.toString() + ' recursively')
+                }
+            })
         }
 
-        if (Config.hashtypes.includes('video240') && data.spritehash) {
-            for await (const file of IPFS.add(urlSource(Config.gateway + '/ipfs/' + data.ipfs240hash, { recursive: true }), { trickle: true })) {
-                console.log('pinned ' + file.cid.toString() + ' recursively')
-            }
+        if (Config.hashtypes.includes('video240') && data.ipfs240hash) {
+            downloadVideo(data.ipfs240hash,async () => {
+                for await (const file of IPFS.add(globSource(data.ipfs240hash, { recursive: true }), { trickle: true })) {
+                    console.log('pinned ' + file.cid.toString() + ' recursively')
+                }
+            })
         }
 
         if (Config.hashtypes.includes('video480') && data.spritehash) {
-            for await (const file of IPFS.add(urlSource(Config.gateway + '/ipfs/' + data.ipfs480hash, { recursive: true }), { trickle: true })) {
-                console.log('pinned ' + file.cid.toString() + ' recursively')
-            }
+            downloadVideo(data.ipfs480hash,async () => {
+                for await (const file of IPFS.add(globSource(data.ipfs480hash, { recursive: true }), { trickle: true })) {
+                    console.log('pinned ' + file.cid.toString() + ' recursively')
+                }
+            })
         }
 
         if (Config.hashtypes.includes('video720') && data.spritehash) {
-            for await (const file of IPFS.add(urlSource(Config.gateway + '/ipfs/' + data.ipfs720hash, { recursive: true }), { trickle: true })) {
-                console.log('pinned ' + file.cid.toString() + ' recursively')
-            }
+            downloadVideo(data.ipfs720hash,async () => {
+                for await (const file of IPFS.add(globSource(data.ipfs720hash, { recursive: true }), { trickle: true })) {
+                    console.log('pinned ' + file.cid.toString() + ' recursively')
+                }
+            })
         }
 
-        if (Config.hashtypes.includes('video240') && data.spritehash) {
-            for await (const file of IPFS.add(urlSource(Config.gateway + '/ipfs/' + data.ipfs1080hash, { recursive: true }), { trickle: true })) {
-                console.log('pinned ' + file.cid.toString() + ' recursively')
-            }
+        if (Config.hashtypes.includes('video1080') && data.spritehash) {
+            downloadVideo(data.ipfs1080hash,async () => {
+                for await (const file of IPFS.add(globSource(data.ipfs1080hash, { recursive: true }), { trickle: true })) {
+                    console.log('pinned ' + file.cid.toString() + ' recursively')
+                }
+            })
         }
     } else if (data.imghash) {
         // image/thumbnail upload
         if (Config.hashtypes.includes('images') && data.imgtype === 'images') {
-            for await (const file of IPFS.add(urlSource(Config.gateway + '/ipfs/' + data.imghash, { recursive: true }), { trickle: true })) {
-                console.log('pinned ' + file.cid.toString() + ' recursively')
-            }
+            downloadVideo(data.imghash,async () => {
+                for await (const file of IPFS.add(globSource(data.imghash, { recursive: true }), { trickle: true })) {
+                    console.log('pinned ' + file.cid.toString() + ' recursively')
+                }
+            })
         } else if (Config.hashtypes.includes('thumbnails') && data.imgtype === 'thumbnails') {
-            for await (const file of IPFS.add(urlSource(Config.gateway + '/ipfs/' + data.imghash, { recursive: true }), { trickle: false })) {
-                console.log('pinned ' + file.cid.toString() + ' recursively')
-            }
+            downloadVideo(data.imghash,async () => {
+                for await (const file of IPFS.add(globSource(data.imghash, { recursive: true }), { trickle: false })) {
+                    console.log('pinned ' + file.cid.toString() + ' recursively')
+                }
+            })
         }
     } else if (Config.hashtypes.includes('subtitles') && data.hash) {
         // subtitle upload
-        for await (const file of IPFS.add(urlSource(Config.gateway + '/ipfs/' + data.hash, { recursive: true }), { trickle: false })) {
-            console.log('pinned ' + file.cid.toString() + ' recursively')
-        }
+        downloadVideo(data.hash,async () => {
+            for await (const file of IPFS.add(globSource(data.hash, { recursive: true }), { trickle: false })) {
+                console.log('pinned ' + file.cid.toString() + ' recursively')
+            }
+        })
     }
 })
+
+function downloadVideo(hash,cb) {
+    let src = Config.gateway + '/ipfs/' + hash
+    let download = wget.download(src,hash,{})
+    download.on('error',(e) => {
+        if (e) console.log('Download error',e)
+    })
+    download.on('start',(filesize) => {
+        console.log('File download begin (' + filesize + ')')
+    })
+    download.on('end',() => {
+        cb()
+    })
+}
